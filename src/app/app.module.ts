9c248ca58e8c807e4716638f846f2e070676ee38
Jeff Nyalik <jeffnyak@gmail.com>
2021-01-19 08:30:42 +0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequestResetPasswordComponent } from './components/password/request-reset-password/request-reset-password.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    RequestResetPasswordComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule,
